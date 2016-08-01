d17fdebad89ca6c48ccede9b0e87406164dfdb99
codenroll <codenroll@codenroll.mynet>
2016-08-01 14:22:38 +0300

    first section in the homepage

--- wp-content/themes/yadlashiryon/home.php
+++ wp-content/themes/yadlashiryon/home.php
@@ -4,43 +4,62 @@
  */
 get_header();
 ?>
-<div class="<?php the_container(); ?>">
-
-	<div class="jumbotron marg-top">
-		<h1>Welcome to Scratch!</h1>
-		<h2>Theme for developers who want to start from scratch.</h2>
-		<p>Version: 1.0.0 | Author: Amit Moreno</p>
-		<p style="margin-bottom: 0"><small>This theme is Free to use under the <a href="http://www.gnu.org/licenses/gpl-2.0.html" target="_blank">GNU General Public License</a>.</small></p>
-	</div>
-	
-	<div class="row">
-		<div class="col-sm-4">
-			<h2 class="h1 marg-bottom">What this theme includes?</h2>
-		</div>
-		<div class="col-sm-8">
-			<div class="panel panel-default pad">
-				<div class="row">
-					<div class="col-sm-6">
-						<h3 class="nomarg-top">Basic Utilities</h3>
-						<ul>
-							<li>Bootstrap 3.3.5</li>
-							<li>Font Awesome 4.4.0</li>
-						</ul>
+<div>
+	<section>
+		<div class="<?php the_container(); ?>">
+		<h1 class="text-center">asdasdas</h1>
+			<div class="row">
+				<div class="col-md-3">
+					<div class="icon">
+						ICON
 					</div>
-					<div class="col-sm-6">
-						<h3 class="nomarg-top">Environment</h3>
-						<ul>
-							<li>Options Panel</li>
-							<li>Security & Performance addons</li>
-							<li>Sass compatibility + Compass compatibility</li>
-							<li>Helper Classes in CSS</li>
-							<li>Useful Functions</li>
-							<li>Extra Quicktags & Shortcodes</li>
-						</ul>
+					<h2 class="text-center">אטרקציות</h2>
+					<h2 class="text-center">ופעילויות חוץ</h2>
+					<p>
+						לורם איפסום דולור איס מט דלמורט סנווון
+					</p>
+					<p>
+						מג'סיר ורונט מגדון שלמיר, מקיסטח דלמון.
+					</p>
+				</div>
+				<div class="col-md-3">
+					<div class="icon">
+						ICON
 					</div>
+					<h2>מרכז האירועים</h2>
+					<p>
+						לורם איפסום דולור איס מט דלמורט סנווון
+					</p>
+					<p>
+						מג'סיר ורונט מגדון שלמיר, מקיסטח דלמון.
+					</p>
+				</div>
+				<div class="col-md-3">
+					<div class="icon">
+						ICON
+					</div>
+					<h2>פעילויות חינוכיות</h2>
+					<p>
+						לורם איפסום דולור איס מט דלמורט סנווון
+					</p>
+					<p>
+						מג'סיר ורונט מגדון שלמיר, מקיסטח דלמון.
+					</p>
+				</div>
+				<div class="col-md-3">
+					<div class="icon">
+						ICON
+					</div>
+					<h2 class="text-center">אטרקציות</h2>
+					<h2 class="text-center">ופעילויות חוץ</h2>
+					<p>
+						לורם איפסום דולור איס מט דלמורט סנווון
+					</p>
+					<p>
+						מג'סיר ורונט מגדון שלמיר, מקיסטח דלמון.
+					</p>
 				</div>
 			</div>
-		</div>
+		</section>
 	</div>
-</div>
 <?php get_footer();?>
