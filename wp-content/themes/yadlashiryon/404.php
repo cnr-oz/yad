7351c3b8bb58bb53d150b41f8b1e7ab296b73521
codenroll <codenroll@codenroll.mynet>
2016-08-01 12:40:11 +0300

    first init

---
+++ wp-content/themes/yadlashiryon/404.php
@@ -0,0 +1,11 @@
+<?php get_header();?>
+<div class="<?php the_container(); ?> marg-top marg-bottom">
+	<div class="row marg-top marg-bottom">
+		<div class="col-sm-push-3 col-sm-6 text-center marg-top marg-bottom">
+			<h1><?php _e('404 - Page not found', 'scratch'); ?></h1>
+			<hr />
+			<h2><?php _e('The page you are looking for does not exist or has been removed.', 'scratch'); ?></h2>
+		</div>
+	</div>
+</div>
+<?php get_footer();?>
